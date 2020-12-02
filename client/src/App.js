00474c4db0dab8a9afc8c93dbcd4c88dcbf81b40
Maher Wardak <maher.wardak@gmail.com>
2020-12-02 13:39:58 -0500

    files updated

--- client/src/App.js
+++ client/src/App.js
@@ -15,23 +15,7 @@ import Style from "./Style.css";
 
 
 
-// const express = require("express");
-// const path = require("path");
 
-// const app = express();
-
-// app.use("/", express.static(path.join(__dirname, "client/build")));
-
-// app.listen(3000);
-
-
-
-// CREATIND API ENDPOINTS
-// 1. Get all messages - GET: "api/messages"
-// 2. Create a chat message - POST: "api/message"
-// 3. Get all users - GET: "api/users"
-// 4. Create a user - POST:"api/users"
-// 5. GEt a single user = GET:"api/users/{id}"
 
 const App = () => {
   // const [users, setUsers] = useState([]);
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// const express = require("express");
-// const path = require("path");
+const express = require("express");
+const path = require("path");
 
-// const app = express();
+const app = express();
 
-// app.use("/", express.static(path.join(__dirname, "client/build")));
+app.use("/", express.static(path.join(__dirname, "client/build")));
 
-// app.listen(3000);
+app.listen(3000);
 
 
 
