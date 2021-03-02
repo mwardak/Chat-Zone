42d793cbef293eda8722e4a425e26ab0e8c4c44a
Maher Wardak <maher.wardak@gmail.com>
2021-03-02 10:05:49 -0500

    created chatpage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,11 @@
-import React, { useState, useRef, useEffect } from "react";
-import axios from "axios";
+
 import Style from "./Style.css";
-import Users from "./components/Users";
-import Messages from "./components/Messages";
-import InputMessage from "./components/InputMessage";
 import LoginForm from "./components/LoginForm";
 import ChatPage from "./components/ChatPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import {
-//   Container,
-//   Col,
-//   Row,
-//   InputGroup,
-//   Button,
-//   Form,
-//   FormControl,
-// } from "react-bootstrap";
-
 const App = () => {
-  const [users, setUsers] = useState([]);
-
-  const [messages, setMessages] = useState([]);
-
-  const fetchUser = async () => {
-    const userResponse1 = await axios.get("/api/users");
-
-    setUsers(userResponse1.data);
-  };
-
-  const fetchMessage = async () => {
-    const messageResponse1 = await axios.get("/api/messages");
-
-    setMessages(messageResponse1.data);
-  };
-
-  useEffect(() => {
-    fetchUser();
-    fetchMessage();
-  }, []);
-
-  let textInputRef = useRef();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessages([{ text: textInputRef.current.value, name: "Maher" }]);
-    textInputRef.current.value = "";
-  };
-
   return (
     <Router>
       <Switch>
--- client/src/components/ChatPage.js
+++ client/src/components/ChatPage.js
@@ -1,30 +1,56 @@
-
-import React from "react";
-import Users from "./components/Users";
-import Messages from "./components/Messages";
-import InputMessage from "./components/InputMessage";
+import axios from "axios";
+import React, { useState, useRef, useEffect }  from "react";
+import Users from "./Users";
+import Messages from "./Messages";
+import InputMessage from "./InputMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const ChatPage = ({  }) => {
+const ChatPage = ({}) => {
+  const [users, setUsers] = useState([]);
+
+  const [messages, setMessages] = useState([]);
+
+  const fetchUser = async () => {
+    const userResponse1 = await axios.get("/api/users");
+
+    setUsers(userResponse1.data);
+  };
+
+  const fetchMessage = async () => {
+    const messageResponse1 = await axios.get("/api/messages");
+
+    setMessages(messageResponse1.data);
+  };
+
+  useEffect(() => {
+    fetchUser();
+    fetchMessage();
+  }, []);
+
+  let textInputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessages([{ text: textInputRef.current.value, name: "Maher" }]);
+    textInputRef.current.value = "";
+  };
   return (
-
-
-
-<div className="page-content page-container" id="page-content">
-          <div className="padding">
-            <div className="container d-flex justify-content-center">
-              <Users users={users} />
-              <Messages messages={messages} />
-            </div>
-
-            <div className="container d-flex justify-content-center">
-              <InputMessage
-                handleSubmit={handleSubmit}
-                textInputRef={textInputRef}
-              />
-            </div>
-          </div>
+    <div className="page-content page-container" id="page-content">
+      <div className="padding">
+        <div className="container d-flex justify-content-center">
+          <Users users={users} />
+          <Messages messages={messages} />
         </div>
-  )};
 
-  export default ChatPage;
+        <div className="container d-flex justify-content-center">
+          <InputMessage
+            handleSubmit={handleSubmit}
+            textInputRef={textInputRef}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
