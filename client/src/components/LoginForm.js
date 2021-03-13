94edd4a032717e4c9f9ccfdc6794ac822e72784c
Maher Wardak <maher.wardak@gmail.com>
2021-03-12 20:08:35 -0500

    WIP login"

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,12 +1,24 @@
 import React from "react";
+import React, { useState, useRef, useEffect }  from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const LoginForm = ({  }) => {
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
+
+  let textInputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail([{ email: textInputRef.current.value}]);
+    textInputRef.current.value = "";
+
   return (
     <form className="container">
       <div className="form-group">
         <label>Email Address</label>
         <input
+          handleSubmit={handleSubmit}
           type="email"
           className="form-control"
           id="email"
